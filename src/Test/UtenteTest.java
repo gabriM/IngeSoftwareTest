3d3f6c66c048baa292bf67e95c8c33efc6f8fbff
MatteoGusmini <45758666+MatteoGusmini@users.noreply.github.com>
2019-05-31 13:53:04 +0200

    Test completi tranne metodi con Input

---
+++ src/Test/UtenteTest.java
@@ -0,0 +1,42 @@
+package Test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import Codice.*;
+
+public class UtenteTest {
+
+	@Test
+	public void testConfrontaUtenteTrue() {
+		Utente u1=new Utente("MaTteo");
+		Utente u2=new Utente("matteo");
+		assertTrue(u1.confrontaUtente(u2));
+		
+	}
+	
+	@Test
+	public void testConfrontaUtenteFalse() {
+		Utente u1=new Utente("Matteo");
+		Utente u2=new Utente("Gabriele");
+		assertFalse(u1.confrontaUtente(u2));
+		
+	}
+	
+	@Test
+	public void testConfrontaUtenteStringaTrue() {
+		Utente u1=new Utente("MaTteo");
+		assertTrue(u1.confrontaUtenteStringa("matteo"));
+		
+	}
+	
+	@Test
+	public void testConfrontaUtenteStringaFalse() {
+		Utente u1=new Utente("MaTteo");
+		assertFalse(u1.confrontaUtenteStringa("Gabriele"));
+		
+	}
+	
+	
+}
